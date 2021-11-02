8fccc81d38948445e9e6cbe0531c5f5aa1eb098e
zorinmish <48491212+zorinmish@users.noreply.github.com>
2021-11-02 20:58:55 +0200

    Hello world
    SwiftUI

---
+++ TripleS/TripleS/TripleSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripleSApp.swift
+//  TripleS
+//
+//  Created by Mykhailo Zorin on 02.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TripleSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
