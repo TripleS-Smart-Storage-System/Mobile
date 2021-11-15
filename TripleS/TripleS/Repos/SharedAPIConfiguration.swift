9ee6cb1cf531034619cb5bb6200c83368a478296
Andrey Karpenko <ximerg002@gmail.com>
2021-11-15 20:01:08 +0200

    Added SharedAPIConfiguration

---
+++ TripleS/TripleS/Repos/SharedAPIConfiguration.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+public enum SharedAPIConfiguration {
+    
+    static let baseUrl: String = "https://triples-api.azurewebsites.net"
+    
+    static let api: String = "api"
+    static let account: String = "Account"
+    static let product: String = "Product"
+    static let role: String = "Role"
+    static let unit: String = "Unit"
+    
+    static let login: String = "login"
+    static let register: String = "register"
+    static let roles: String = "roles"
+    static let units: String = "units"
+    static let users: String = "users"
+}
